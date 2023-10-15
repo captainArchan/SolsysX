dbf8d1f8f0f453ba0410ed62f89f216293d3f0c4
Captain <archan.phalanuphat@gmail.com>
2023-10-15 15:58:33 +0700

    update

--- SolsysXExpo/App.js
+++ SolsysXExpo/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MyNavigator from './navigator/MyNavigator';
+import HomePage2 from './database/connectData';
 
 export default function App() {
   return (
-    <MyNavigator/>
+    <HomePage2/>
   );
 }
 
--- SolsysXExpo/database/connectData.js
+++ SolsysXExpo/database/connectData.js
@@ -1,3 +1,39 @@
-import firebase from "firebase/compat";
-import { Component } from "react";
+import firebase from "./firebaseDB";
+import { Component, useState, useEffect } from "react";
+import { View, Text } from "react-native";
 
+export default function HomePage2() {
+    const planetCollection = firebase.firestore().collection("planet");
+    const [dataPlanet, setDataPlanet] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const querySnapshot = await planetCollection.get();
+                const all_data = [];
+                querySnapshot.forEach((res) => {
+                      console.log("res: ", res);
+                    const { code, name, credit } = res.data();
+                    all_data.push({
+                        key: res.id,
+                        code,
+                        name,
+                        credit,
+                    });
+                });
+                console.log(all_data); // Log the data from Firestore
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+    return (
+        <View>
+            <Text>123</Text>
+        </View>
+    );
+}
